3d25e6bb6dd104b5555ba321b5ba6f253506256e
Anton Nemtsev <nemtsev.online@gmail.com>
2021-02-15 16:57:06 +0300

    Added Getters and Setters

--- src/main/java/ru/netology/domain/GeoInfo.java
+++ src/main/java/ru/netology/domain/GeoInfo.java
@@ -3,4 +3,20 @@ package ru.netology.domain;
 public class GeoInfo {
     private String type;
     private String coordinates;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- src/main/java/ru/netology/domain/VewsInfo.java
+++ src/main/java/ru/netology/domain/VewsInfo.java
@@ -2,4 +2,12 @@ package ru.netology.domain;
 
 public class VewsInfo {
     private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
