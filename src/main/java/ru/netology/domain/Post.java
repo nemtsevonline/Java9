f6890f6dcd1ac82e5178f81d35c8945e5e14b741
Anton Nemtsev <nemtsev.online@gmail.com>
2021-02-15 14:38:35 +0300

    v1.

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String date;
+    private String time;
+  private String textPost;
+    private String imgGeneralUrl;
+    private Boolean like;
+    private Boolean repost;
+    private CommentsInfo commentsInfo;
+}
